54f38b18cb7a7309b9db156c51b6bac22a7bdd21
Gabriel Merola <gabrielmerola12@gmail.com>
2025-02-11 22:46:18 -0300

    Update role check in PrivateRoutesCOM to allow access for STUDENT users

--- src/utils/PrivateRoutesCOM.js
+++ src/utils/PrivateRoutesCOM.js
@@ -8,7 +8,7 @@ export default function PrivateRoutesCO () {
     try {
         const tokenData = jwtDecode(token);
 
-        if (token && (tokenData.role === "PROFESSOR") && (tokenData.status === "ACTIVE")) {
+        if (token && (tokenData.role === "STUDENT") && (tokenData.status === "ACTIVE")) {
             return <Outlet />;
         } else {
             return <Navigate to='/error' />;
